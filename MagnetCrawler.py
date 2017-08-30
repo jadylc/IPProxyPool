f6d6e9348e787e8162cc13cc9eed3e1068f61143
liuhan <hanliu@tujia.com>
2017-08-30 17:09:01 +0800

    加入线程池，并行

--- MagnetCrawler.py
+++ MagnetCrawler.py
@@ -9,6 +9,8 @@ from random import choice
 
 import requests
 from bs4 import BeautifulSoup
+import threadpool
+
 import config
 from db.DataStore import sqlhelper
 
@@ -45,86 +47,83 @@ def get_proxies_random(proxy_list):
 
 target_url = 'https://btso.pw/search/'
 
-def search_magnet(list_code):
-    socket.setdefaulttimeout(5)
-    urls =[]
-    for code in list_code:
-        urls.append(target_url+code+'R')
-        for index, url in enumerate(urls):
-            res = requests.get(url, headers=config.get_header(), proxies=get_proxies(index, proxy_list)).content
-            if res == None:
-                res = requests.get(url, headers=config.get_header(), proxies=get_proxies(index+1, proxy_list)).content
-                if res == None:
-                    code_notfind.append(url)
-                else:
-                    prase(url,res)
-            else:
-                prase(url,res)
+def search_magnet(code):
+    socket.setdefaulttimeout(3)
+    url = target_url+code+'R'
+    res = requests.get(url, headers=config.get_header(), proxies=get_proxies_random(proxy_list))
+    if res.status_code != 200:
+        res1 = requests.get(url, headers=config.get_header(), proxies=get_proxies_random(proxy_list))
+        if res1.status_code != 200:
+            logging.error('connect refused')
+    else:
+        prase(code,res.content)
 
-def search_magnet2(list_code):
-    urls = []
-    for url in code_notfind:
-        url = url[:-1]
-        urls.append(url)
-    socket.setdefaulttimeout(5)
-    for index, url in enumerate(urls):
-        res = requests.get(url, headers=config.get_header(), proxies=get_proxies(index, proxy_list)).content
-        if res == None:
-            res = requests.get(url, headers=config.get_header(), proxies=get_proxies(index+1, proxy_list)).content
-            if res == None:
-                code_notfind_final.append(url)
-            else:
-                prase2(url,res)
-        else:
-            prase2(url,res)
 
-def prase(url,res):
+def search_magnet2(code):
+    socket.setdefaulttimeout(3)
+    url2 = target_url + code
+    res = requests.get(url2, headers=config.get_header(), proxies=get_proxies_random(proxy_list))
+    if res.status_code != 200:
+        res1 = requests.get(url2, headers=config.get_header(), proxies=get_proxies_random(proxy_list))
+        if res1.status_code != 200:
+            logging.error('connect refused')
+    else:
+        prase2(code,res.content)
+
+def prase(code,res):
     soup = BeautifulSoup(res.decode('UTF-8'), 'html.parser')
     if soup.find('div', class_='data-list'):
         url1 = (soup.find('div', class_='data-list')).a['href']
-        get_detail(url1)
+        list_magnet_ch.append(get_detail(url1))
     else:
-        code_notfind.append(url)
+        code_notfind.append(code[:-1])
 
-def prase2(url,res):
+def prase2(code,res):
     soup = BeautifulSoup(res.decode('UTF-8'), 'html.parser')
     if soup.find('div', class_='data-list'):
         url1 = (soup.find('div', class_='data-list')).a['href']
-        get_detail(url1)
+        list_magnet.append(get_detail(url1))
     else:
-        code_notfind_final.append(url)
+        code_notfind_final.append(code)
 
 
 def get_detail(url):
     res = requests.get(url, headers=config.get_header(), proxies=get_proxies_random(proxy_list)).content
-    if res == None:
+    if res is None:
         res = requests.get(url, headers=config.get_header(), proxies=get_proxies_random(proxy_list)).content
-        if res == None:
+        if res is None:
             res = requests.get(url, headers=config.get_header(), proxies=get_proxies_random(proxy_list)).content
-            if res == None:
+            if res is None:
                 print('get_detail error')
             else:
-                prase_detail(url, res)
+                return prase_detail(res)
         else:
-            prase_detail(url, res)
+            return prase_detail(res)
     else:
-        prase_detail(url, res)
+        return prase_detail(res)
 
 
-def prase_detail(url,res):
+def prase_detail(res):
     soup = BeautifulSoup(res.decode('UTF-8'), 'html.parser')
     url2 = str(soup.textarea.get_text())
     magnet = url2.replace(';', '')
-    list_magnet.append(magnet)
+    return magnet
 
 
 if __name__ == '__main__':
     code_notfind = []
     code_notfind_final = []
     list_magnet = []
-    proxy_list = sqlhelper.select()
-    search_magnet(get_code())
-    search_magnet2(code_notfind)
+    list_magnet_ch = []
+    proxy_list = sqlhelper.select(10)
+    pool = threadpool.ThreadPool(10)
+    reqs = threadpool.makeRequests(search_magnet, get_code())
+    [pool.putRequest(req) for req in reqs]
+    pool.wait()
+    reqs2 = threadpool.makeRequests(search_magnet2, code_notfind)
+    [pool.putRequest(req) for req in reqs2]
+    pool.wait()
     print(code_notfind)
     print(code_notfind_final)
     print(list_magnet)
+    print(list_magnet_ch)
